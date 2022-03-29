9fb8e66e2fec12d485b718ec849affe575f1ef5f
natnael-OQP <makiya112545@gmail.com>
2022-03-29 17:48:37 +0300

    login route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+
+const users = [
+    {
+        id: "1",
+        username: "natnael",
+        password: "natnael-oqp",
+        isAdmin: true,
+    },
+    {
+        id: "2",
+        username: "jonas",
+        password: "jonas",
+        isAdmin: false,
+    },
+];
+
+// middleware
 app.use(express.json());
+
+app.post("/api/login",async(req, res) => {   
+    const user = users.find(user =>{
+        return user.username === req.body?.username && user.password === req.body?.password;
+    })
+    if(user){
+        res.json(user)
+    }else{
+        res.status(400).json("wrong credential ")
+    }
+})
+
 
 app.listen(5000, () => console.log("Backend server is running!"));
