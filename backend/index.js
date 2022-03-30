858230217fe95d21fd51005548878a54410a8194
natnael-OQP <makiya112545@gmail.com>
2022-03-30 23:40:05 +0300

    login completed

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,73 @@
-const express = require("express");
-const app = express();
-const jwt = require("jsonwebtoken");
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
 
 const users = [
     {
-        id: "1",
-        username: "natnael",
-        password: "natnael-oqp",
+        id: '10',
+        username: 'natnael',
+        password: 'natnael',
         isAdmin: true,
     },
     {
-        id: "2",
-        username: "jonas",
-        password: "jonas",
+        id: '0',
+        username: 'jonas',
+        password: 'jonas',
         isAdmin: false,
     },
-];
+]
 
 // middleware
-app.use(express.json());
+app.use(express.json())
 
-app.post("/api/login",async(req, res) => {   
-    const user = users.find(user =>{
-        return user.username === req.body?.username && user.password === req.body?.password;
+// login
+app.post('/api/login', async (req, res) => {
+    const user = users.find((user) => {
+        return (
+            user.username === req.body?.username &&
+            user.password === req.body?.password
+        )
     })
-    if(user){
-        res.json(user)
-    }else{
-        res.status(400).json("wrong credential ")
+
+    if (user) {
+        // generate token
+        const accessToken = jwt.sign({ id: user.id }, 'Me89qrh9sd', {
+            expiresIn: '2s',
+        })
+        res.json({
+            username: user.username,
+            accessToken,
+        })
+    } else {
+        res.status(400).json('wrong credential ')
     }
 })
 
+// verify middleware
+const verify = (req, res, next) => {
+    const { authorization } = req.headers
+    if (authorization) {
+        const token = authorization.split(' ')[1]
+        console.log(authorization)
+        jwt.verify(token, 'Me89qrh9sd', (err, data) => {
+            if (err) {
+                return res.status(403).json(err)
+            }
+            req.user = data
+            next()
+        })
+    } else {
+        res.status(401).json('you are not authenticated')
+    }
+}
 
-app.listen(5000, () => console.log("Backend server is running!"));
+// delete user
+app.delete('/api/users/:userId', verify, (req, res) => {
+    if (req.user.id === req.params.userId) {
+        return res.status(200).json('user has been deleted')
+    } else {
+        return res.status(200).json('you are not allowed to deleted this user')
+    }
+})
+
+app.listen(5000, () => console.log('Backend server is running!'))
